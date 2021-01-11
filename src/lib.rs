d00b83465216ec99b45fc734082cb4d48213727c
loxygenk <loxygen.k@gmail.com>
2021-01-11 14:08:49 +0900

    ✨ Export plugin

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use autoclip_core::{AutoclipPlugin, PluginRegistrar};
+
+autoclip_core::export_plugin!("pfngp", AutoclipPluginPfngp);
+
+pub struct AutoclipPluginPfngp;
+
+impl AutoclipPlugin for AutoclipPluginPfngp {
+    fn on_clip(&self, contents: &str) -> Option<String> {
+        Some(format!("{} ... OK", contents))
     }
 }
